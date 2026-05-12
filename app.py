a54f49d3987e8fee907fb3f96597c83b1c196cae
makswwy <maksimkaklinovio@mail.ru>
2026-05-12 17:03:56 +0300

    Update app.py

--- app.py
+++ app.py
@@ -532,5 +532,5 @@ def health():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", "3000"))
+    port = int(os.environ.get("PORT", "7777"))
     app.run(host="0.0.0.0", port=port)
